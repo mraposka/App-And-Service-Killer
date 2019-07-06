8864a0e83ba64816fdf628df416e0e018d8d7d8e
mraposka <kadircankinsiz@gmail.com>
2019-07-06 23:06:20 +0300

    Kill selected apps and services

---
+++ AppKiller/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace AppKiller
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.searchApp = new System.Windows.Forms.Button();
+            this.selectApp = new System.Windows.Forms.Button();
+            this.showApps = new System.Windows.Forms.Button();
+            this.deleteApp = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.killToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.directorySearcher1 = new System.DirectoryServices.DirectorySearcher();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // searchApp
+            // 
+            this.searchApp.Location = new System.Drawing.Point(12, 3);
+            this.searchApp.Name = "searchApp";
+            this.searchApp.Size = new System.Drawing.Size(120, 23);
+            this.searchApp.TabIndex = 1;
+            this.searchApp.Text = "Search For App";
+            this.searchApp.UseVisualStyleBackColor = true;
+            this.searchApp.Click += new System.EventHandler(this.searchApp_Click);
+            // 
+            // selectApp
+            // 
+            this.selectApp.Location = new System.Drawing.Point(12, 61);
+            this.selectApp.Name = "selectApp";
+            this.selectApp.Size = new System.Drawing.Size(120, 23);
+            this.selectApp.TabIndex = 2;
+            this.selectApp.Text = "Select or Write=>";
+            this.selectApp.UseVisualStyleBackColor = true;
+            this.selectApp.Click += new System.EventHandler(this.selectApp_Click);
+            // 
+            // showApps
+            // 
+            this.showApps.Location = new System.Drawing.Point(13, 32);
+            this.showApps.Name = "showApps";
+            this.showApps.Size = new System.Drawing.Size(120, 23);
+            this.showApps.TabIndex = 4;
+            this.showApps.Text = "Show";
+            this.showApps.UseVisualStyleBackColor = true;
+            this.showApps.Click += new System.EventHandler(this.showApps_Click);
+            // 
+            // deleteApp
+            // 
+            this.deleteApp.Location = new System.Drawing.Point(139, 32);
+            this.deleteApp.Name = "deleteApp";
+            this.deleteApp.Size = new System.Drawing.Size(120, 23);
+            this.deleteApp.TabIndex = 5;
+            this.deleteApp.Text = "Delete";
+            this.deleteApp.UseVisualStyleBackColor = true;
+            this.deleteApp.Click += new System.EventHandler(this.deleteApp_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(138, 64);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(592, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Kill";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.killApp_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(674, 65);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(37, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "All";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "ASd";
+            this.notifyIcon1.BalloonTipTitle = "fghj";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick_1);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.killToolStripMenuItem,
+            this.settingsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 92);
+            // 
+            // killToolStripMenuItem
+            // 
+            this.killToolStripMenuItem.Name = "killToolStripMenuItem";
+            this.killToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.killToolStripMenuItem.Text = "Kill";
+            this.killToolStripMenuItem.Click += new System.EventHandler(this.killToolStripMenuItem_Click);
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            this.settingsToolStripMenuItem.Click += new System.EventHandler(this.settingsToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click_1);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 31;
+            this.listBox1.Location = new System.Drawing.Point(13, 88);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(705, 345);
+            this.listBox1.TabIndex = 11;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(138, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(119, 20);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Search For Services";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(660, 393);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(58, 40);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Minimize";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(263, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(461, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Before \"Delete\" or \"Select or Write\" select an app or service on list. Or you can" +
+    " write and select.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(263, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(359, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "For select app or service, click \"Search For App\" or \"Search For Services\"";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(263, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(308, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Don\'t write like that => \"notepad.exe\" - Write like this \"notepad\"";
+            // 
+            // directorySearcher1
+            // 
+            this.directorySearcher1.ClientTimeout = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(730, 442);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.deleteApp);
+            this.Controls.Add(this.showApps);
+            this.Controls.Add(this.selectApp);
+            this.Controls.Add(this.searchApp);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button searchApp;
+        private System.Windows.Forms.Button selectApp;
+        private System.Windows.Forms.Button showApps;
+        private System.Windows.Forms.Button deleteApp;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem killToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.DirectoryServices.DirectorySearcher directorySearcher1;
+    }
+}
+
---
+++ AppKiller/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.ServiceProcess;
+
+namespace AppKiller
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        [DllImport("user32.dll")]
+        public static extern int FindWindow(string ClassName, string WindowName);
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(int hWnd, uint Msg, int wParam, int lParam);
+        private NotifyIcon trayIcon;
+        private ContextMenu trayMenu;
+        public const int WM_SYSCOMMAND = 0x0112;
+        public const int SC_CLOSE = 0xF060;
+        string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\AppKiller.txt";
+        public string GetAllApps()
+        {
+            string allApp="";
+            using (var reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line == "")
+                    { }
+                    else if (line != "")
+                    { allApp += line + System.Environment.NewLine ; }
+                }
+            }
+            return allApp;
+        }
+
+        public void UpdateListBox()
+        {
+            listBox1.Items.Clear();
+            using (var reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line == "")
+                    { }
+                    else if (line != "")
+                    { listBox1.Items.Add(line); }
+                }
+            }
+        }
+
+        private void searchApp_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            Process[] processes = Process.GetProcesses();
+            foreach (Process p in processes)
+            {
+                    listBox1.Items.Add(p.ProcessName);
+            }
+        }
+
+        private void showApps_Click(object sender, EventArgs e)
+        {
+            UpdateListBox();
+        }
+
+        private void deleteApp_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Remove(listBox1.SelectedItem);
+            string text = "";
+            foreach (var item in listBox1.Items)
+            {
+                text += item.ToString() +Environment.NewLine;
+            }
+            File.WriteAllText(path, text);
+        }
+
+        private void selectApp_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "") { 
+            string selectedAppName = listBox1.SelectedItem.ToString();
+            string allApp = GetAllApps();
+            File.WriteAllText(path, allApp + selectedAppName);
+                //UpdateListBox();
+            }
+            else
+            {
+                string selectedAppName = textBox1.Text;
+                string allApp = GetAllApps();
+                File.WriteAllText(path, allApp + selectedAppName);
+            }
+            textBox1.Clear();
+            UpdateListBox();
+        }
+        private void ServiceKillerFunc(string serviceName)
+        {
+            try
+            {
+                ServiceController service = new ServiceController(serviceName);
+                service.Stop();
+                var timeout = new TimeSpan(0, 0, 5); // 5seconds
+                service.WaitForStatus(ServiceControllerStatus.Stopped, timeout);
+
+            }
+            catch (Exception exc)
+            {
+                // MessageBox.Show("k"+exc.Message);
+            }
+        }
+        private void AppKillerFunc(string appName)
+        {
+            try
+            {
+                foreach (System.Diagnostics.Process myProc in System.Diagnostics.Process.GetProcesses())
+                {
+                    if (myProc.ProcessName == appName)
+                    {
+                        myProc.Kill();
+                    }
+                }
+            }
+            catch(Exception exc)
+            {
+                // MessageBox.Show("q"+exc.Message);
+            }
+
+        }
+
+        private void killApp_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int HWND = FindWindow(null, "My Window");//window title
+
+                SendMessage(HWND, WM_SYSCOMMAND, SC_CLOSE, 0);
+                if (checkBox1.Checked == false)
+                {
+                    string selectedAppName = listBox1.SelectedItem.ToString();
+                    AppKillerFunc(selectedAppName);
+                    ServiceKillerFunc(selectedAppName);
+                }
+                else
+                {
+                    for (int i = 0; i <= (listBox1.Items.Count-1); i++)
+                    {
+                            string selectedAppName = listBox1.Items[i].ToString();
+                            AppKillerFunc(selectedAppName);
+                            ServiceKillerFunc(selectedAppName);
+                    }
+                }
+                
+            }
+            catch (Exception exc)
+            {
+                //MessageBox.Show("Hata:" + exc.Message);
+            }
+
+            UpdateListBox();
+        }
+       
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+        private void notifyIcon1_MouseDoubleClick_1(object sender, MouseEventArgs e)
+        {
+          
+        }
+
+        private void killToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            checkBox1.Checked = true;
+            killApp_Click(null, null);
+        }
+
+        private void exitToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            notifyIcon1.BalloonTipTitle = "Info";
+            notifyIcon1.BalloonTipText = "Double click to maximize.";
+            notifyIcon1.ContextMenuStrip = contextMenuStrip1;
+            notifyIcon1.Visible = true;
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            UpdateListBox();
+           this.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            ServiceController[] oaServiceController = ServiceController.GetServices(Environment.MachineName);
+            foreach (ServiceController sc in oaServiceController)
+                listBox1.Items.Add(sc.DisplayName);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            notifyIcon1.BalloonTipTitle = "Info";
+            notifyIcon1.BalloonTipText = "Double click to maximize.";
+            notifyIcon1.ContextMenuStrip = contextMenuStrip1;
+            notifyIcon1.Visible = true;
+            listBox1.Items.Clear();
+        }
+
+        private void settingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Show();
+            notifyIcon1.Visible = false;
+            listBox1.Items.Clear();
+        }
+        
+    }
+}
